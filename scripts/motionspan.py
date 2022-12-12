ea8e845e0fc477af6e84ec66faf72b215858514b
Soojin Choi <97choisj@kaist.ac.kr>
2022-12-12 15:14:39 +0900

    organized and added functions on motionspan script
    - pose 단위 span 계산을 위한 함수 추가 
    - 기존 함수 내에 is_sum_all 파라미터 추가 
    - 불필요한 함수 주석처리

--- scripts/motionspan.py
+++ scripts/motionspan.py
@@ -3,23 +3,23 @@ import numpy as np
 from fairmotion.ops import conversions
 from fairmotion.core.motion import Pose, Motion
 
-# get all joint positions of all frames
-def get_all_joint_positions(motion, frame_start, frame_end, joint_idx):
-    joint_ps = []
-    
-    for frame in range(frame_start, frame_end):
-        pose = Motion.get_pose_by_frame(motion, frame)
-        global_p = get_joint_position(pose, joint_idx)
-        joint_ps.append(np.array(global_p))
-    
-    return np.array(joint_ps)                               # [nof, 3]
-
 # get global joint positions of a single joint 
 def get_joint_position(pose, joint_idx):
     global_T = pose.get_transform(joint_idx, local=False)
     global_p = conversions.T2p(global_T)
     
     return global_p
+
+# get all joint positions of selected frames 
+def get_all_joint_positions(motion, frame_start, frame_end, joint_idx):
+    joint_ps = []
+    
+    for frame in range(frame_start, frame_end):
+        pose = Motion.get_pose_by_frame(motion, frame)
+        global_p = get_joint_position(pose, joint_idx)
+        joint_ps.append(global_p)
+    
+    return np.array(joint_ps)                               # [nof, 3]
 
 # get c_i
 def get_joint_centroid(joint_ps):
@@ -28,41 +28,92 @@ def get_joint_centroid(joint_ps):
 
     return centroid
 
-# compute span for a single joint (output: [1, ])
-def extract_joint_span(motion, frame_start, frame_end, joint_idx):
-    joint_span = 0    
+# get c_i for all joints 
+def get_all_centroids(motion, frame_start, frame_end):
+    centroids=[]
+    skel = motion.poses[0].skel
+    
+    for joint in skel.joints:
+        joint_idx = skel.get_index_joint(joint)
+        joint_ps = get_all_joint_positions(motion, frame_start, frame_end, joint_idx)
+        centroid = get_joint_centroid(joint_ps)
+        centroids.append(centroid) 
+    
+    return centroids
+
+# compute span of a single joint 
+def extract_joint_span(motion, frame_start, frame_end, joint_idx, is_sum_all):
+    joint_span = []    
     joint_ps = get_all_joint_positions(motion, frame_start, frame_end, joint_idx)
     centroid = get_joint_centroid(joint_ps)
     
     for frame in range(frame_start, frame_end):
-        pose = Motion.get_pose_by_frame(motion, frame)
-        joint_p = get_joint_position(pose, joint_idx)
-        magnitude = np.linalg.norm(joint_p - centroid)     # distance between joint position & centroid 
-        joint_span += magnitude
+        joint_p = joint_ps[frame]
+        magnitude = np.linalg.norm(joint_p - centroid)     # ! distance between joint position & centroid 
+        joint_span.append(magnitude)
     
-    return joint_span
+    if is_sum_all is True:
+        return np.sum(np.array(joint_span), axis=0)        # [1,]
+    else:
+        return np.array(joint_span)                        # [nof,]
 
-# compute span for all joints (output: [noj, ])
-def extract_motion_span(motion, frame_start, frame_end):
+# compute span of all joints 
+def extract_motion_span(motion, frame_start, frame_end, is_sum_all):
     motion_span = []
     skel = motion.poses[0].skel
     include_root = True
     
     for joint in skel.joints:
-        
         joint_idx = skel.get_index_joint(joint)
-        
+
         if include_root is False:
             if joint_idx == 0: 
                 continue
         
-        joint_span = extract_joint_span(motion, frame_start, frame_end, joint_idx)
-        
-        motion_span.append(joint_span)
+        joint_span = extract_joint_span(motion, frame_start, frame_end, joint_idx, is_sum_all=True)        
+        motion_span.append(joint_span)                      # [noj,]
     
-    print("motion_span shape : ", np.array(motion_span).shape)
+    if is_sum_all is True:
+        return np.sum(np.array(motion_span), axis=0)        # [1,]
+    else:
+        return np.array(motion_span)                        # [noj,]
+
+# compute span for a single pose in a single frame 
+def extract_pose_span(pose, centroids):
+    pose_span = 0
+    skel = pose.skel
+    
+    for joint in skel.joints:
+        joint_idx = skel.get_index_joint(joint)
+
+        joint_p = get_joint_position(pose, joint_idx)
+        centroid = centroids[joint_idx]
+        
+        magnitude = np.linalg.norm(joint_p - centroid)     # ! distance between joint position & centroid 
+        pose_span += magnitude
+    
+    return np.array(pose_span)        #[1,]
+    
+
+# compute span of all joints with respect to time-axis (output: [nof,])
+def extract_frame_wise_pose_span(motion, frame_start, frame_end):
+    motion_span = []
+    centroids = get_all_centroids(motion, frame_start, frame_end)
+    
+    for frame in range(frame_start, frame_end):
+        pose = Motion.get_pose_by_frame(motion, frame)    
+        pose_span = extract_pose_span(pose, centroids)
+        motion_span.append(pose_span)
     
     return np.array(motion_span)
-
     
+# comnpute span of selected joints 
+# def extract_selected_joints_span(motion, frame_start, frame_end, joint_idxes):
+#     joints_span = []
+#     skel = motion.poses[0].skel
     
+#     for joint_idx in joint_idxes:
+#         joint_span = extract_joint_span(motion, frame_start, frame_end, joint_idx)
+#         joints_span.append(joint_span)
+    
+#     return np.array(joints_span)                           # [n, ] (n = num of seleced joints)
